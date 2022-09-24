2a57cde1e544e1032adab31de2fabf5177202a73
jyr <jyr@wavers.co.kr>
2022-09-24 15:58:21 +0900

    반환 타입으로 함수 타입 사용하기, 궁금증 해소하기: 코틀린의 람다는 클로저다, 궁금증 해소하기: 람다 vs 익명 내부 클래스

--- src/main/kotlin/SimVillage.kt
+++ src/main/kotlin/SimVillage.kt
@@ -4,11 +4,12 @@ fun main(args: Array<String>) {
 //        println("$numBuildings 채의 건물이 추가됨")
 //        "SimVillage 방문을 환영합니다, $playerName 님! (copyright $currentYear)"
 //    }
-    runSimulation("김선달", ::printConstructionCost) { playerName, numBuildings ->
-        val currentYear = 2019
-        println("$numBuildings 채의 건물이 추가됨")
-        "SimVillage 방문을 환영합니다, $playerName 님! (copyright $currentYear)"
-    }
+//    runSimulation("김선달", ::printConstructionCost) { playerName, numBuildings ->
+//        val currentYear = 2019
+//        println("$numBuildings 채의 건물이 추가됨")
+//        "SimVillage 방문을 환영합니다, $playerName 님! (copyright $currentYear)"
+//    }
+    runSimulation()
 //    println({
 //        val currentYear = 2019
 //        "SimVillage 방문을 환영합니다, 촌장님! (copyright $currentYear)"
@@ -17,10 +18,26 @@ fun main(args: Array<String>) {
 //    runSimulation("김선달", greetingFunction)
 }
 
-inline fun runSimulation(playerName: String, costPrinter: (Int) -> Unit, greetingFunction: (String, Int) -> String) {
-    val numBuildings = (1..3).shuffled().last() // 1, 2, 3 중 하나를 무작위로 선택한다.
-    costPrinter(numBuildings)
-    println(greetingFunction(playerName, numBuildings))
+//inline fun runSimulation(playerName: String, costPrinter: (Int) -> Unit, greetingFunction: (String, Int) -> String) {
+//    val numBuildings = (1..3).shuffled().last() // 1, 2, 3 중 하나를 무작위로 선택한다.
+//    costPrinter(numBuildings)
+//    println(greetingFunction(playerName, numBuildings))
+//}
+
+fun runSimulation() {
+    val greetingFunction = configreGreetingFunction()
+    println(greetingFunction("김선달"))
+}
+
+fun configreGreetingFunction(): (String) -> String {
+    val structureType = "병원"
+    var numBuildings = 5
+    return { playerName: String ->
+        val currentYear = 2019
+        numBuildings += 1
+        println("$numBuildings 채의 $structureType 이 추가됨")
+        "SimVillage 방문을 환영합니다, $playerName! (copyright $currentYear)"
+    }
 }
 
 fun printConstructionCost(numBuildings: Int) {
