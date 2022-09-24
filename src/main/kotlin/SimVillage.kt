f3eac5261e7f6451efe0542f33e6e3cb67ccac01
jyr <jyr@wavers.co.kr>
2022-09-24 15:45:34 +0900

    함수를 인자로 받는 함수 정의하기, 단축 문법, 인라인 함수로 만들기, 함수 참조

--- src/main/kotlin/SimVillage.kt
+++ src/main/kotlin/SimVillage.kt
@@ -1,5 +1,10 @@
 fun main(args: Array<String>) {
-    val greetingFunction = { playerName: String, numBuildings: Int ->
+//    val greetingFunction = { playerName: String, numBuildings: Int ->
+//        val currentYear = 2019
+//        println("$numBuildings 채의 건물이 추가됨")
+//        "SimVillage 방문을 환영합니다, $playerName 님! (copyright $currentYear)"
+//    }
+    runSimulation("김선달", ::printConstructionCost) { playerName, numBuildings ->
         val currentYear = 2019
         println("$numBuildings 채의 건물이 추가됨")
         "SimVillage 방문을 환영합니다, $playerName 님! (copyright $currentYear)"
@@ -8,5 +13,17 @@ fun main(args: Array<String>) {
 //        val currentYear = 2019
 //        "SimVillage 방문을 환영합니다, 촌장님! (copyright $currentYear)"
 //    }())
-    println(greetingFunction("김선달", 2))
+//    println(greetingFunction("김선달", 2))
+//    runSimulation("김선달", greetingFunction)
+}
+
+inline fun runSimulation(playerName: String, costPrinter: (Int) -> Unit, greetingFunction: (String, Int) -> String) {
+    val numBuildings = (1..3).shuffled().last() // 1, 2, 3 중 하나를 무작위로 선택한다.
+    costPrinter(numBuildings)
+    println(greetingFunction(playerName, numBuildings))
+}
+
+fun printConstructionCost(numBuildings: Int) {
+    val cost = 500
+    println("건축 비용: ${cost * numBuildings}")
 }
